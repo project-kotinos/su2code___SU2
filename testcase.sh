a9a714e94c4f23c7c91c1378018602e117c23880
wmuenigneer <adam.d.bell@icloud.io>
2019-07-24 12:40:01 -0700

    Move everything to a script

--- testcase.sh
+++ testcase.sh
@@ -1,3 +1,10 @@
+#! /bin/bash
+echo "Entering script"
+git clone -b develop https://github.com/su2code/TestCases.git ./TestData
+cp -R ./TestData/* ./TestCases/
+    # Get the tutorial cases
+git clone -b develop https://github.com/su2code/su2code.github.io ./Tutorials 
+    # Enter the SU2/TestCases/ directory, which is now ready to run
 cd TestCases/
+echo "Directory moved & now running script"
 python serial_regression.py
-echo "Running serial_regression"
